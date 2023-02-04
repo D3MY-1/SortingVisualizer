39ab95bda57908a6f22d4ecfcd72b2429b3224ec
Артур Малиновски <arturmalinnka@gmail.com>
2023-02-04 23:31:55 +0200

    Rewrited class IntElem to ElemTracker to get rid of index sorting nonsense. Fixed bug comparisons wasn't showing.

--- Source.cpp
+++ Source.cpp
@@ -45,7 +45,7 @@ namespace Test
         }
     }
 
-    void BubblesortB(std::vector<int>& vec)
+    void BubblesortB(std::vector<Visualizer::IntTracked>& vec)
     {
         bool sorted = false;
 
@@ -79,7 +79,7 @@ namespace Test
         }
     }
 
-    void ShakerSortB(std::vector<int>& vec)
+    void ShakerSortB(std::vector<Visualizer::IntTracked>& vec)
     {
         bool sorted = false;
 
@@ -121,7 +121,7 @@ namespace Test
         }
     }
 
-    void GnomeSortB(std::vector<int>& vec)
+    void GnomeSortB(std::vector<Visualizer::IntTracked>& vec)
     {
         int pos = 0;
         int temp = 0;
@@ -148,7 +148,7 @@ namespace Test
         }
     }
 
-    void SelectionSortB(std::vector<int>& vec)
+    void SelectionSortB(std::vector <Visualizer::IntTracked> & vec)
     {
 
         int it = 0;
@@ -159,7 +159,7 @@ namespace Test
 
 
 
-        int temp = 0;
+        Visualizer::IntTracked temp = 0;
 
         while (!sorted)
         {
@@ -194,17 +194,17 @@ namespace Test
         }
     }
 
-    void InsertionSortB(std::vector<int>& vec)
+    void InsertionSortB(std::vector<Visualizer::IntTracked>& vec)
     {
-        int i, key, j;
+        int i, j,key;
         for (i = 1; i < vec.size(); i++)
         {
-            key = vec[i];
+            key = int(vec[i]);
             j = i - 1;
 
             
 
-            while (j >= 0 && int(vec[j]) > key)
+            while (j >= 0 && vec[j] > vec[i])
             {
                 Visualizer::HighlightRed(j);
                 Visualizer::Update();
@@ -212,29 +212,29 @@ namespace Test
                 j = j - 1;
                 
             }
-            vec[j + 1] = key;
+            vec[j + 1] = vec[i];
             Visualizer::isInPlace(j + 1);
             
         }
         Visualizer::Update();
     }
 
-    void RadixSortB(std::vector<int>& vec)
+    void RadixSortB(std::vector<Visualizer::IntTracked>& vec)
     {
-        auto countingSort = [](std::vector<int>& vec, int exp) {
+        auto countingSort = [](std::vector<Visualizer::IntTracked>& vec, int exp) {
             std::vector<int> outp(vec.size());
             int i, count[10] = { 0 };
 
             for (i = 0; i < vec.size(); i++)
-                count[(vec[i] / exp) % 10]++;
+                count[(int(vec[i]) / exp) % 10]++;
 
             for (i = 1; i < 10; i++)
                 count[i] += count[i - 1];
 
             for (i = vec.size() - 1; i >= 0; i--)
             {
-                outp[count[(vec[i] / exp) % 10] - 1] = vec[i];
-                count[(vec[i] / exp) % 10]--;
+                outp[count[(int(vec[i]) / exp) % 10] - 1] = int(vec[i]);
+                count[(int(vec[i]) / exp) % 10]--;
             }
 
             for (i = 0; i < vec.size(); i++)
@@ -246,7 +246,7 @@ namespace Test
                 
         };
 
-        int max = *std::max_element(vec.begin(), vec.end());
+        int max = int( * std::max_element(vec.begin(), vec.end()));
 
         for (int exp = 1; max / exp > 0; exp *= 10)
             countingSort(vec, exp);
@@ -344,6 +344,11 @@ int main(int argc, char* argv[]) {
         case 8:
             Visualizer::Start(std::stable_sort);
         }
+
+        auto stats = Visualizer::GetStats();
+        std::cout << "\nArray was sorted!!!\nStats\nNumber of comparisons is " << stats.first << "\nNumber of Array changes is " << stats.second << "\nPress Enter To Continue...";
+        getchar();
+
         system("cls");
     }
 
